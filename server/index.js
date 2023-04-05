e7744e3825088c9079340b96af53253904b629e1
Tswap007 <tswap700@gmail.com>
2023-04-05 17:25:47 +0100

    Generate the merkle Proof from the client and have the server verify the Proof also handled error case of undefined name.

--- server/index.js
+++ server/index.js
@@ -8,15 +8,20 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = '3385f85a7f32152aec10c295f84e9c5532d36b1ada84776dc3cafbfdfdf1336a';
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
-
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  const name = body.name;
+  const proof = body.proof;
+  // TODO: prove that a name is in the list
+  if (name === undefined) {
+    res.status(400).send("ERROR: Empty Name detected");
+    return;
+  }
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+  if (isInTheList) {
     res.send("You got a toy robot!");
   }
   else {
